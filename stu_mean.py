55c0afead63a708e81240e626811d7aa1dc7fb31
Tina <tchen9@stuy.edu>
2017-10-17 21:14:57 -0400

    updated

--- stu_mean.py
+++ stu_mean.py
@@ -3,11 +3,19 @@ import sqlite3
 db = sqlite3.connect("students.db")
 c = db.cursor()
 
-#Look up students' grades
+#Create list with (mark, id) of each mark from courses
 command = "SELECT mark, id FROM courses"
 c.execute(command)
 gradeList =  c.fetchall()
-print(gradeList)
+#print(gradeList)
+
+#Create list with (name, id) from people
+command = "SELECT name, id FROM people"
+c.execute(command)
+names = c.fetchall()
+#print names
+
+#Get student's averages
 def getAverage(studentid):
 	avgsum = 0;
 	count = 0;
@@ -17,5 +25,18 @@ def getAverage(studentid):
 			count += 1
 	return avgsum / count
 
-print getAverage(1)
-		 
+#print getAverage(1)
+
+#Create table
+def createTable():
+        c.execute("drop table peeps_avg")
+        command = "CREATE TABLE peeps_avg (id INTEGER, avg INTEGER);"
+        c.execute(command)
+        for each in names:
+                avg = getAverage(each[1])
+                command = 'INSERT INTO peeps_avg VALUES(' + str(each[1]) + ',' + str(avg) + ');'
+                #print(command)
+                c.execute(command)
+                
+createTable()
+        
